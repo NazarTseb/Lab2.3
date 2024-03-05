8694d371ca2759d43a9ce6a597ea0d3d1f2b96b8
NazarTseb <nazartsebryk1@gmail.com>
2024-03-05 14:30:57 +0200

    Lab2.3

---
+++ Лаб 2.3/Лаб 2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        List<int> numbers = new List<int> { 5, -15, 25, 10, 100, 50, 75, 15 };
+
+        var NeededNumbers = numbers.Where(n => n >= 10 && n <= 99).OrderBy(n => n);
+
+        Console.WriteLine("Додатні двозначні числа, відсортовані по зростанню:");
+        foreach (var number in NeededNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
